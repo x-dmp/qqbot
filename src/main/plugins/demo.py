15560879bf74b3111638d4905c72125077dd1bdd
x-dmp <2416335716@qq.com>
2021-03-29 13:58:29 +0800

    test

--- src/main/plugins/demo.py
+++ src/main/plugins/demo.py
@@ -3,7 +3,11 @@ from nonebot import on_command, CommandSession
 @on_command('setu', aliases=('涩图', '色图'))
 async def setu(session: CommandSession):
     print('执行setu')
-    print(session.current_arg_text)
+    keyword = session.current_arg_text
+    apikey = '95462828605c9b29d66cb3'
+    size1200 = True
+    r18 = 0
+
     await session.send('收到涩图命令')
 
 
