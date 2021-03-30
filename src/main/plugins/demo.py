68b6124d7f63b39f9896c1263060c858aa974e13
x-dmp <2416335716@qq.com>
2021-03-30 14:45:06 +0800

    test

--- src/main/plugins/demo.py
+++ src/main/plugins/demo.py
@@ -6,7 +6,7 @@ import sys
 from src.main.utils.getPic import get_pic, get_pic_from_local
 
 
-@on_command('setu', aliases=('涩图', '色图', '来一张涩图', '来一张色图', '来点涩图', '来点色图'))
+@on_command('setu', aliases=('涩图', '色图'))
 async def setu(session: CommandSession):
     print('执行setu'+session.current_arg_text)
     res = ''
@@ -15,13 +15,26 @@ async def setu(session: CommandSession):
         res = '恭喜你获得老婆一只！'
     else:
         try:
-            src, _ = get_pic(r18=0, keyword=session.current_arg_text)
+            src, quota = get_pic(r18=0, keyword=session.current_arg_text)
+            if src == '-1':
+                src = get_pic_from_local()
+                print('api内部错误')
+            elif src == '401':
+                src = get_pic_from_local()
+                print('由于不规范的操作而被拒绝调用')
+            elif src == '403':
+                src = get_pic_from_local()
+                print('找不到符合关键字的色图')
+            elif src == '429':
+                src = get_pic_from_local()
+                print('达到调用额度限制')
+            await session.send('剩余服务次数：' + str(quota))
             res = '恭喜你获得' + session.current_arg_text.strip() +'老婆一只！'
         except Exception:
             src = get_pic_from_local()
             res = '网络受到神秘的非物质力量干扰，故老婆丢失，这边给您换了一个'
     await session.send(MessageSegment.image(src))
-    await session.send(res)
+    await session.send(res+src)
 
 
 # @on_natural_language(keywords={'涩图'}, only_to_me=True)
--- src/main/utils/getPic.py
+++ src/main/utils/getPic.py
@@ -14,16 +14,26 @@ def get_pic(r18=0, keyword='') -> tuple:
     response = requests.get(url)
     data = response.json()
     quota = data['quota']
-    if quota == 0:
-        raise Exception
+    code = data['code']
+    if code == 429:
+        return '429', quota
+    elif code == -1:
+        return '-1', quota
+    elif code == 401:
+        return '401', quota
+    elif code == 404:
+        return '404', quota
+    elif code == 403:
+        return '403', quota
     pic_url = data['data'][0]['url']
     img_name = pic_url.split('/')[-1]
     loc = opt['cache_dir'] + img_name
     if not os.path.exists(loc):
+        print("本地无图片，开始下载")
         img = requests.get(pic_url)
         img_cache = Image.open(BytesIO(img.content))
         img_cache.save(loc, quality=80)
-        print("本地无图片，开始下载")
+        print("下载完成")
     return loc, quota
 
 
