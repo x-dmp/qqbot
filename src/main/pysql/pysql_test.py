d737dba6d8d40ac0d0c31a61541c4ccf3aef7bea
x-dmp <2416335716@qq.com>
2021-04-01 09:29:20 +0800

    code update

--- src/main/pysql/pysql_test.py
+++ src/main/pysql/pysql_test.py
@@ -1,9 +1,46 @@
 import pymysql
 from src.main.utils.direction import get_dir
 
+'''
+更新数据库      write_db(uid, dir, tag) 返回Ture False
+根据uid查dir   find_dir_by_id(uid)     返回dirs[]
+根据tag查uid   find_ids_by_tag(tag)    返回ids[]
+'''
+def write_db(uid, dir, tag):
+   flag = True
+   if find_tf_by_id(uid):
+      flag = update_pic(uid, dir, tag)
+   else:
+      flag = insert_pic(uid, dir, tag)
+   return flag
+
+
+def find_tf_by_id(uid):
+   flag = True
+   db = pymysql.connect(host='localhost', user='root', passwd='root', db='setu_t_p', port=3306, charset='utf8')
+   cursor = db.cursor()
+   sql = """SELECT * FROM setupic WHERE uid=%s"""
+   sqlargs = []
+   ids = []
+   sqlargs.append(uid)
+   try:
+      cursor.execute(query=sql, args=sqlargs)
+      print('执行sql语句')
+      results = cursor.fetchall()
+      for row in results:
+         ids.append(row[0])
+   except:
+      print('错误')
+   cursor.close()
+   db.close()
+   if len(ids)==0:
+      flag=False
+   return flag
+
+
+
 def insert_pic(uid, dir, tag):
    flag = True
-
    db = pymysql.connect(host='localhost', user='root', passwd='root', db='setu_t_p', port=3306, charset='utf8')
    cursor = db.cursor()
    sql = """INSERT INTO setupic(uid,dir,tag)VALUES(%s,%s,%s)"""
@@ -11,53 +48,87 @@ def insert_pic(uid, dir, tag):
    sqlargs.append(uid)
    sqlargs.append(dir)
    sqlargs.append(tag)
-
    try:
-      # 执行sql语句
       cursor.execute(query=sql,args=sqlargs)
       print('执行sql语句')
       # 提交到数据库执行
       db.commit()
       print('提交数据库')
    except:
-      # 如果发生错误则回滚
       db.rollback()
       print('错误')
       flag=False
    cursor.close()
    db.close()
-
    return flag
 
 
-def find_ids_by_tag(tag):
-   # 打开数据库连接
+def update_pic(uid, dir, tag):
+   flag = True
    db = pymysql.connect(host='localhost', user='root', passwd='root', db='setu_t_p', port=3306, charset='utf8')
-
-   # 使用cursor()方法获取操作游标
    cursor = db.cursor()
+   sql="""UPDATE setupic SET dir=%s,tag=%s WHERE uid=%s"""
+   sqlargs=[]
+   sqlargs.append(dir)
+   sqlargs.append(tag)
+   sqlargs.append(uid)
+   try:
+      cursor.execute(query=sql,args=sqlargs)
+      print('执行sql语句')
+      # 提交到数据库执行
+      db.commit()
+      print('提交数据库')
+   except:
+      db.rollback()
+      print('错误')
+      flag=False
+   return flag
 
-   # SQL 插入语句
+
+def find_dir_by_id(uid):
+   db = pymysql.connect(host='localhost', user='root', passwd='root', db='setu_t_p', port=3306, charset='utf8')
+   cursor = db.cursor()
+   sql = """SELECT * FROM setupic WHERE uid=%s"""
+   sqlargs = []
+   dirs = []
+   sqlargs.append(uid)
+   try:
+      cursor.execute(query=sql, args=sqlargs)
+      print('执行sql语句')
+      results = cursor.fetchall()
+      for row in results:
+         dirs.append(row[1])
+   except:
+      print('错误')
+   cursor.close()
+   db.close()
+   print(dirs)
+   return dirs
+
+def find_ids_by_tag(tag):
+   db = pymysql.connect(host='localhost', user='root', passwd='root', db='setu_t_p', port=3306, charset='utf8')
+   cursor = db.cursor()
    sql = """SELECT * FROM setupic WHERE tag LIKE %s"""
    tag = '%' + tag + '%'
    sqlargs = []
    ids = []
    sqlargs.append(tag)
    try:
-      # 执行sql语句
       cursor.execute(query=sql, args=sqlargs)
       print('执行sql语句')
-
-      # 获取结果
       results = cursor.fetchall()
       for row in results:
          ids.append(row[0])
    except:
-      # 如果发生错误
       print('错误')
-
    cursor.close()
    db.close()
    print(ids)
    return ids
 
+
+if __name__ == '__main__':
+   # print(update_pic('123456789', 'test', 'wdnmd'))
+   # find_dir_by_id('123456789')
+   # print(find_tf_by_id('12345679'))
+   write_db('12345', 'zyxwvut', 'nsdd')
